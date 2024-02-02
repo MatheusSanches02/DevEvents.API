c7893c47c9aed025bff0d2973b5b2a75b82485eb
MatheusSanches02 <ma12sanches@gmail.com>
2024-02-01 22:02:29 -0300

    inclusão de tratamento de erros

--- AwesomeDevEvents.API/Controllers/DevEventsController.cs
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -19,105 +19,153 @@ namespace AwesomeDevEvents.API.Controllers
         [HttpGet("getAll")]
         public IActionResult GetAll() 
         {
-            var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
+            try
+            {
+                var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
 
-            return Ok(devEvents);
+                return Ok(devEvents);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("getById/{id}")]   
         public IActionResult GetById(Guid id) 
         {
-            var devEvents = _context.DevEvents
-                .Include(de => de.Speakers)
-                .SingleOrDefault(d => d.Id == id);
-
-            if (devEvents == null)
+            try
             {
-                return NotFound();
-            }
+                var devEvents = _context.DevEvents
+                    .Include(de => de.Speakers)
+                    .SingleOrDefault(d => d.Id == id);
 
-            return Ok(devEvents);
+                if (devEvents == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(devEvents);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost("create")]
         public IActionResult Post(DevEvent devEvent) 
         {
-            _context.DevEvents.Add(devEvent);
+            try
+            {
+                _context.DevEvents.Add(devEvent);
 
-            _context.SaveChanges();
+                _context.SaveChanges();
 
-            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id}, devEvent);
+                return CreatedAtAction(nameof(GetById), new { id = devEvent.Id}, devEvent);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut("update/{id}")]
         public IActionResult Update(Guid id, DevEvent devEvent)
         {
-            var devEvents = _context.DevEvents.SingleOrDefault(d => d.Id == id);
-
-            if (devEvents == null)
+            try
             {
-                return NotFound();
+                var devEvents = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+                if (devEvents == null)
+                {
+                    return NotFound();
+                }
+
+                devEvents.Update(devEvent.Title, devEvent.Description, devEvent.StartDate, devEvent.EndDate);
+
+                _context.DevEvents.Update(devEvent);
+                _context.SaveChanges();
+
+                return NoContent();
             }
-
-            devEvents.Update(devEvent.Title, devEvent.Description, devEvent.StartDate, devEvent.EndDate);
-
-            _context.DevEvents.Update(devEvent);
-            _context.SaveChanges();
-
-            return NoContent();
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpDelete("delete/{id}")]
         public IActionResult Delete(Guid id)
         {
-            var devEvents = _context.DevEvents.SingleOrDefault(d => d.Id == id);
-
-            if (devEvents == null)
+            try
             {
-                return NotFound();
+                var devEvents = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+                if (devEvents == null)
+                {
+                    return NotFound();
+                }
+
+                devEvents.Delete();
+
+                _context.SaveChanges();
+
+                return NoContent();
             }
-
-            devEvents.Delete();
-
-            _context.SaveChanges();
-
-            return NoContent();
+            catch(Exception ex) 
+            { 
+                return BadRequest(ex.Message); 
+            }
         }
 
         [HttpPost("{id}/include-speakers")]
         public IActionResult PostSpeaker(Guid id,DevEventSpeaker speaker) 
         {
-            speaker.DevEventId = id;
-
-            var devEvents = _context.DevEvents.Any(d => d.Id == id);
-
-            if (!devEvents)
+            try
             {
-                return NotFound();
+                speaker.DevEventId = id;
+
+                var devEvents = _context.DevEvents.Any(d => d.Id == id);
+
+                if (!devEvents)
+                {
+                    return NotFound();
+                }
+
+                _context.DevEventsSpeakers.Add(speaker);
+                _context.SaveChanges();
+
+                return NoContent();
             }
-
-            _context.DevEventsSpeakers.Add(speaker);
-            _context.SaveChanges();
-
-            return NoContent();
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
         
         [HttpDelete("{speakerId}/delete-speakers")]
         public IActionResult DeleteSpeaker(Guid speakerId) 
         {
-
-            var speaker = _context.DevEventsSpeakers.SingleOrDefault(deSpeaker =>  deSpeaker.Id == speakerId);
-
-
-            if (speaker == null)
+            try
             {
-                return NotFound();
+                var speaker = _context.DevEventsSpeakers.SingleOrDefault(deSpeaker =>  deSpeaker.Id == speakerId);
+
+
+                if (speaker == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Remove(speaker);
+                _context.SaveChanges();
+
+                return NoContent();
             }
-
-            _context.Remove(speaker);
-            _context.SaveChanges();
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
